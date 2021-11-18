94b326b65c01d25d33ffc285c88825e065d0b789
Bartol Deak <b@bdeak.net>
2021-11-18 15:49:58 +0100

    ostali izbornici

--- Program.cs
+++ Program.cs
@@ -50,7 +50,126 @@ namespace PopisStanovnistva
                         }, izbornik);
                         break;
                     case 11:
-                        Console.WriteLine(popis);
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 12:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 13:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 2:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 3:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 4:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 5:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 6:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 7:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 8:
+                        izbornik = Odabir(new string[] {
+                            "Uredi OIB stanovnika",
+                            "Uredi ime i prezime stanovnika",
+                            "Uredi datum rođenja",
+                        }, izbornik);
+                        break;
+                    case 81:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 82:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 83:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 9:
+                        izbornik = Odabir(new string[] {
+                            "Postotak nezaposlenih (od 0 do 23 godine i od 65 do 100 godine) i postotak zaposlenih (od 23 do 65 godine)",
+                            "Ispis najčešćeg imena i koliko ga stanovnika ima",
+                            "Ispis najčešćeg prezimena i koliko ga stanovnika ima",
+                            "Ispis datum na koji je rođen najveći broj ljudi i koji je to datum",
+                            "Ispis broja ljudi rođenih u svakom od godišnjih doba",
+                            "Ispis najmlađeg stanovnika",
+                            "Ispis najstarijeg stanovnika",
+                            "Prosječan broj godina",
+                            "Medijan godina",
+                        }, izbornik);
+                        break;
+                    case 91:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 92:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 93:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 94:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 95:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 96:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 97:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 98:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 99:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 10:
+                        izbornik = Odabir(new string[] {
+                            "Abecedno po prezimenima",
+                            "Po datumu rođenja",
+                            "Silazno",
+                        }, izbornik);
+                        break;
+                    case 101:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 102:
+                        Console.WriteLine("TODO");
+                        izbornik = Odabir(Array.Empty<string>(), izbornik);
+                        break;
+                    case 103:
+                        Console.WriteLine("TODO");
                         izbornik = Odabir(Array.Empty<string>(), izbornik);
                         break;
                 }
@@ -67,12 +186,12 @@ namespace PopisStanovnistva
             else Console.WriteLine("0 - Povratak u glavni izbornik");
 
             Console.Write("Odaberite akciju: ");
-            int odabir = int.Parse(Console.ReadLine());
-            while (odabir < 0 || odabir > akcije.Length)
+            bool success = int.TryParse(Console.ReadLine(), out int odabir);
+            while (odabir < 0 || odabir > akcije.Length || !success)
             {
                 Console.WriteLine("Akcija mora biti jedan od brojeva u listi.");
                 Console.Write("Odaberite akciju: ");
-                odabir = int.Parse(Console.ReadLine());
+                success = int.TryParse(Console.ReadLine(), out odabir);
             }
 
             if (prosli_odabir > 0 && odabir != 0)
